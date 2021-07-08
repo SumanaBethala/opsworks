b502637ece3f237fa82fff7abfb3bfc8c7bfa95a
SumanaBethala <51401976+SumanaBethala@users.noreply.github.com>
2021-07-09 01:27:16 +0530

    shine changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Simple PHP App</title>
+        <font>Times New Romans</font>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #EE82EE;}</style>
